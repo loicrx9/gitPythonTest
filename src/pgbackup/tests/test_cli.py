c66f41f4b9723d073900960ceb9390c37e645eb5
loicrx9 <loicrossetbv@gmail.com>
2019-02-07 10:48:42 +0100

    Miracle

--- src/pgbackup/tests/test_cli.py
+++ src/pgbackup/tests/test_cli.py
@@ -1,11 +1,12 @@
 import pytest
+
 from pgbackup import cli
 
-url = "postgres://bob@example.com:5432/db_one"
+url = "postgres://bob:password@example.com:5432/db_one"
 
 def test_parser_without_driver():
     """
-    without a specified driver
+    Without a specified driver the parser will exit
     """
     with pytest.raises(SystemExit):
         parser = cli.create_parser()
@@ -13,21 +14,21 @@ def test_parser_without_driver():
 
 def test_parser_with_driver():
     """
-    The parser will exit if it receives a driver without a destination
+    The parser will exit if it receives a driver
+    without a destination
     """
-
     parser = cli.create_parser()
     with pytest.raises(SystemExit):
         parser.parse_args([url, "--driver", "local"])
 
 def test_parser_with_driver_and_destination():
     """
-    The parser will not exit if it receives a driver and destination
+    The parser will not exit if it receives a driver
+    with a destination
     """
     parser = cli.create_parser()
 
-    args = parser.parse_args([url, '--driver', 'local', '/some/path'])
+    args = parser.parse_args([url, "--driver", "local", "/some/path"])
 
     assert args.driver == "local"
     assert args.destination == "/some/path"
-
